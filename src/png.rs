f639031db84403223eb933cab166a67897e6404d
YotioSoft <ytani0323@gmail.com>
2023-03-16 18:59:36 +0900

    add PngImage.raw_image & fix png compression

--- src/png.rs
+++ src/png.rs
@@ -4,29 +4,32 @@ use std::io::{Read, Write};
 
 pub struct PngImage {
     pub image: Vec<u8>,
+    pub raw_image: Vec<u8>,
     pub width: usize,
     pub height: usize,
 }
 
 impl PngImage {
-    pub fn new(image: Vec<u8>, width: usize, height: usize) -> Self {
+    pub fn new(image: Vec<u8>, raw_image: Vec<u8>, width: usize, height: usize) -> Self {
         Self {
             image,
+            raw_image,
             width,
             height,
         }
     }
 
     pub fn open(path: &str) -> Result<Self, String> {
-        let mut raw_data = std::fs::File::open(path).map_err(|_| "Failed to open file".to_string())?;
+        let mut file = std::fs::File::open(path).map_err(|_| "Failed to open file".to_string())?;
         let mut buf = Vec::new();
-        raw_data.read_to_end(&mut buf).map_err(|_| "Failed to read file".to_string())?;
+        file.read_to_end(&mut buf).map_err(|_| "Failed to read file".to_string())?;
 
         let image = image::load_from_memory(&buf).map_err(|_| "Failed to open image".to_string())?;
         let (width, height) = (image.width() as usize, image.height() as usize);
 
         Ok(Self {
             image: image.into_bytes(),
+            raw_image: buf,
             width,
             height,
         })
@@ -41,7 +44,7 @@ impl PngImage {
 
     pub fn compress(&mut self) -> Result<(), String> {
         println!("compressing png image...");
-        match oxipng::optimize_from_memory(&self.image, &oxipng::Options::default()) {
+        match oxipng::optimize_from_memory(&self.raw_image, &oxipng::Options::default()) {
             Ok(data) => {
                 self.image = data;
                 Ok(())
