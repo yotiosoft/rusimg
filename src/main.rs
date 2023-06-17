ac6df9e2f5a49ec137da58d8e2575a21c71588b4
YotioSoft <ytani0323@gmail.com>
2023-06-18 01:15:12 +0900

    add ProcessingError that contains RusimgError and std::io::Error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use std::path::{Path, PathBuf};
 use std::fs;
+use std::fmt;
 
 mod parse;
 mod rusimg;
 use glob::glob;
 use parse::ArgStruct;
 use rusimg::RusimgError;
+
+pub enum ProcessingError {
+    RusimgError(RusimgError),
+    IOError(String),
+}
+impl fmt::Display for ProcessingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ProcessingError::RusimgError(e) => write!(f, "{}", e.to_string()),
+            ProcessingError::IOError(e) => write!(f, "{}", e),
+        }
+    }
+}
 
 fn get_files_in_dir(dir_path: &PathBuf) -> Result<Vec<PathBuf>, String> {
     let mut files = fs::read_dir(&dir_path).expect("cannot read directory");
@@ -52,40 +66,43 @@ fn get_files_by_wildcard(source_path: &PathBuf) -> Result<Vec<PathBuf>, String> 
     Ok(ret)
 }
 
-fn process(args: ArgStruct, image_file_path: PathBuf) -> Result<(), RusimgError> {
+fn process(args: &ArgStruct, image_file_path: &PathBuf) -> Result<(), ProcessingError> {
+    let rierr = |e: RusimgError| ProcessingError::RusimgError(e);
+    let ioerr = |e: std::io::Error| ProcessingError::IOError(e.to_string());
+
     // ファイルを開く
-    let mut image = rusimg::open_image(&image_file_path)?;
+    let mut image = rusimg::open_image(&image_file_path).map_err(rierr)?;
 
     // --trim -> トリミング
     if let Some(trim) = args.trim {
         // トリミング
-        rusimg::trim(&mut image, (trim.0.0, trim.0.1), (trim.1.0, trim.1.1))?;
+        rusimg::trim(&mut image, (trim.0.0, trim.0.1), (trim.1.0, trim.1.1)).map_err(rierr)?;
     }
 
     // --resize -> リサイズ
     if let Some(resize) = args.resize {
         // リサイズ
-        rusimg::resize(&mut image, resize)?;
+        rusimg::resize(&mut image, resize).map_err(rierr)?;
     }
 
     // --convert -> 画像形式変換
     if let Some(ref c) = args.destination_extension {
-        let extension = rusimg::convert_str_to_extension(&c).map_err(|e| e.to_string())?;
+        let extension = rusimg::convert_str_to_extension(&c).map_err(rierr)?;
 
         // 変換
-        rusimg::convert(&mut image, &extension)?;
+        rusimg::convert(&mut image, &extension).map_err(rierr)?;
     }
 
     // --grayscale -> グレースケール
     if args.grayscale {
         // グレースケール
-        rusimg::grayscale(&mut image)?;
+        rusimg::grayscale(&mut image).map_err(rierr)?;
     }
 
     // --quality -> 圧縮
     if let Some(q) = args.quality {
         // 圧縮
-        rusimg::compress(&mut image.data, &image.extension, Some(q))?;
+        rusimg::compress(&mut image.data, &image.extension, Some(q)).map_err(rierr)?;
     }
 
     // 出力
@@ -93,32 +110,25 @@ fn process(args: ArgStruct, image_file_path: PathBuf) -> Result<(), RusimgError>
         Some(path) => Some(path),
         None => None,
     };
-    let saved_filepath = rusimg::save_image(output_path, &mut image.data, &image.extension)?;
+    let saved_filepath = rusimg::save_image(output_path, &mut image.data, &image.extension).map_err(rierr)?;
 
     // --delete -> 元ファイルの削除 (optinal)
-    if args.delete && image_file_path != saved_filepath {
-        match fs::remove_file(&image_file_path) {
-            Ok(_) => (),
-            Err(e) => return Err(e.to_string()),
-        }
+    if args.delete && image_file_path != &saved_filepath {
+        fs::remove_file(&image_file_path).map_err(ioerr)?;
     }
 
     // 表示
     if args.view {
-        match rusimg::view(&mut image) {
-            Ok(_) => (),
-            Err(e) => return Err(e.to_string()),
-        }
+        rusimg::view(&mut image).map_err(rierr)?;
     }
+
+    Ok(())
 }
 
 fn main() -> Result<(), String> {
     let args = parse::parser();
 
-    let source_path = match args.souce_path {
-        Some(s) => s,
-        None => PathBuf::from(".")                  // カレントディレクトリ
-    };
+    let source_path = args.souce_path.clone().or(Some(PathBuf::from("."))).unwrap();
     let image_files = if source_path.is_dir() {
         get_files_in_dir(&source_path)?
     }
@@ -129,7 +139,13 @@ fn main() -> Result<(), String> {
     for image_file_path in image_files {
         println!("[Processing: {}]", &Path::new(&image_file_path).file_name().unwrap().to_str().unwrap());
 
-        
+        match process(&args, &image_file_path) {
+            Ok(_) => {},
+            Err(e) => {
+                println!("Error: {}", e.to_string());
+                continue;
+            },
+        }
 
         println!("Done.");
     }
