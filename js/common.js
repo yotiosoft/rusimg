45c76595787dc199ef5497fc672415129ecc0738
yotiosoft <yotio@yotiosoft.com>
2025-05-06 15:18:54 +0900

    filepath

--- js/common.js
+++ js/common.js
@@ -12,22 +12,22 @@ if ((navigator.userAgent.indexOf('iPhone') > 0) || navigator.userAgent.indexOf('
 //共通パーツ読み込み
 $(function() {
     if (window.innerWidth <= 767 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-        $("#header").load("/header_m.html");
-        $("#footer").load("/footer_m.html");
+        $("#header").load("./header_m.html");
+        $("#footer").load("./footer_m.html");
     }
     else {
-        $("#header").load("/header.html");
-        $("#footer").load("/footer.html");
+        $("#header").load("./header.html");
+        $("#footer").load("./footer.html");
     }
     
     window.addEventListener('resize', function(){
         if (window.innerWidth <= 767 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-            $("#header").load("/header_m.html");
-            $("#footer").load("/footer_m.html");
+            $("#header").load("./header_m.html");
+            $("#footer").load("./footer_m.html");
         }
         else {
-            $("#header").load("/header.html");
-            $("#footer").load("/footer.html");
+            $("#header").load("./header.html");
+            $("#footer").load("./footer.html");
         }
     });
 });
